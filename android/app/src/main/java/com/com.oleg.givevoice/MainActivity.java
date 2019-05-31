a6fc4cf6320a9cf45a93862ec30831705eccd641
evgeniy <evgeniy.logvinov@cyoda.com>
2019-05-31 12:22:40 +0300

    Change package name

--- android/app/src/main/java/com/com.oleg.givevoice/MainActivity.java
+++ android/app/src/main/java/com/com.oleg.givevoice/MainActivity.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.com.oleg.givevoice;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "AwesomeProject";
+        return "com.oleg.givevoice";
     }
 }
--- android/app/src/main/java/com/com.oleg.givevoice/MainApplication.java
+++ android/app/src/main/java/com/com.oleg.givevoice/MainApplication.java
@@ -1,9 +1,8 @@
-package com.awesomeproject;
+package com.com.oleg.givevoice;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.dooboolab.RNIap.RNIapPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNIapPackage()
+          new MainReactPackage()
       );
     }
 
