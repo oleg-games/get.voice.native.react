b8add0e3860fd042f70ee39b6bb50f4f4427971e
Evgeniy Logvinov <evgeniy.logvinov@cyoda.com>
2019-06-10 16:46:44 +0300

    typescript files

---
+++ app/src/constants/Storage.tsx
@@ -0,0 +1,6 @@
+export default {
+    TOKEN: 'token',
+    QUESTION: 'question',
+    ANSWER: 'answer',
+    PHONE_NUMBER: 'phoneNumber',
+};
---
+++ app/src/screens/Auth/AuthLoadingScreen.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import {
+    ActivityIndicator,
+    StatusBar,
+    View,
+} from 'react-native';
+import StorageConst from '../../constants/Storage';
+
+export default class AuthLoadingScreen extends React.Component {
+    constructor(props: any) {
+        super(props);
+        this._bootstrapAsync();
+    }
+
+    // Fetch the token from storage then navigate to our appropriate place
+    _bootstrapAsync = async () => {
+        console.log('authLoadingScreen')
+        const token = await AsyncStorage.getItem(StorageConst.TOKEN);
+        console.log('token', token)
+
+        // This will switch to the App screen or Auth screen and this loading
+        // screen will be unmounted and thrown away.
+        // const gVprops: any = this.props; 
+        // gVprops.navigation.navigate(token ? 'App' : 'Auth');
+    };
+
+    // Render any loading content that you like here
+    render() {
+        return (
+            <View>
+                <ActivityIndicator />
+                <StatusBar barStyle="default" />
+            </View>
+        );
+    }
+}
